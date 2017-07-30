3142c50d9d89105f1493eea4e7b31dc910ebb4f3
meishijie <shiji@mm.local>
2017-07-30 20:06:01 +0800

    init

---
+++ src/components/Hello.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello1">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+     <button v-on:click="gr">Greet</button>
+     <my-component></my-component>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      name: 'name的值'
+    }
+  },
+  methods: {
+    gr: function (event) {
+      // `this` 在方法里指当前 Vue 实例
+      alert('Hello ' + this.name + '!')
+      // `event` 是原生 DOM 事件
+      if (event) {
+        alert(event.target.tagName)
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello1">
+    <h1>{{ msg }}</h1>
+    <h2>中文</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data () {
+    return {
+      msg: '订单'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
